985e2eb9016d41414613be011fc560a3ae29a61f
Jose Fco de la Fuente <josefcodelafuente@gmail.com>
2024-11-23 12:24:23 +0100

    feat: env mod demo

--- env.sh
+++ env.sh
@@ -5,17 +5,17 @@ export DEMO="demo"
 export GKE_PROJECT_ID="$DEMO-base-gke" 
 
 # variables de red 
-export GKE_NETWORK_NAME="base-main" 
-export GKE_SUBNET_NAME="base-private" 
-export GKE_ROUTER_NAME="base-router" 
-export GKE_NAT_NAME="base-nat" 
+export GKE_NETWORK_NAME="$DEMO-base-main" 
+export GKE_SUBNET_NAME="$DEMO-base-private" 
+export GKE_ROUTER_NAME="$DEMO-base-router" 
+export GKE_NAT_NAME="$DEMO-base-nat" 
 
 # variables principales 
-export GKE_SA_NAME="gke-worker-nodes-sa" 
+export GKE_SA_NAME="$DEMO-gke-worker-nodes-sa" 
 export GKE_SA_EMAIL="$GKE_SA_NAME@${GKE_PROJECT_ID}.iam.gserviceaccount.com" 
 
 # variables gke 
-export GKE_CLUSTER_NAME="base-gke" 
+export GKE_CLUSTER_NAME="$DEMO-base-gke" 
 export GKE_REGION="us-central1" 
 export GKE_MACHINE_TYPE="e2-standard-2" 
 
