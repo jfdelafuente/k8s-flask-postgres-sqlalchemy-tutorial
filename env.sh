e0a3937d928094c62189e8d0f69f5a95488590b2
Jose Fco de la Fuente <josefcodelafuente@gmail.com>
2024-11-24 17:21:18 +0100

    feat: mod env

--- env.sh
+++ env.sh
@@ -1,8 +1,8 @@
 # variable
-export DEMO="my-app"
+export DEMO="$PROJECT_ID"
 
 # variable global 
-export GKE_PROJECT_ID="$DEMO-base-gke"
+export GKE_PROJECT_ID="$DEMO"
 
 # variables de red 
 export GKE_NETWORK_NAME="$DEMO-base-main" 
@@ -16,7 +16,7 @@ export GKE_SA_EMAIL="$GKE_SA_NAME@${GKE_PROJECT_ID}.iam.gserviceaccount.com"
 
 # variables gke 
 export GKE_CLUSTER_NAME="$DEMO-base-gke" 
-export GKE_REGION="us-central1" 
+export GKE_REGION="$REGION" 
 export GKE_MACHINE_TYPE="e2-standard-2" 
 
 # variables de cliente kubectl 
@@ -24,15 +24,6 @@ export USE_GKE_GCLOUD_AUTH_PLUGIN="True"
 export KUBECONFIG=~/.kube/gcp/$GKE_REGION-$GKE_CLUSTER_NAME.yaml
 
 export CLOUD_BILLING_ACCOUNT="0152B2-7E0055-F23A0D"
-
-echo $DEMO
-echo $GKE_PROJECT_ID
-echo $GKE_NETWORK_NAME
-echo $GKE_SUBNET_NAME
-echo $GKE_ROUTER_NAME
-echo $GKE_NAT_NAME
-
-echo $GKE_SA_EMAIL
 
 
 
